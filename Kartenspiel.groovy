5fba526cac21459961c0090dd9ac8b83d461a0e1
Axel Klinger <axel.klinger@web.de>
2017-07-02 20:43:19 +0200

    def entfernt

--- Kartenspiel.groovy
+++ Kartenspiel.groovy
@@ -1,24 +1,24 @@
 
 
 class Kartenspiel {
-  static def blatt = ["Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass",
+  static blatt = ["Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass",
                "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo Ass",
                "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz Ass",
                "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik Ass"]
 
-  static def spieler1 = []
-  static def spieler2 = []
-  static def spieler3 = []
+  static spieler1 = []
+  static spieler2 = []
+  static spieler3 = []
 
   static Random random = new Random()
 
-  static def ziehen() {
+  static ziehen() {
     def karte = blatt.get(random.nextInt(blatt.size()))
     blatt = blatt - karte
     return karte
   }
 
-  static def geben() {
+  static geben() {
     10.times {
       spieler1 << ziehen()
       spieler2 << ziehen()
@@ -26,13 +26,14 @@ class Kartenspiel {
     }
   }
 
-  static def spielen(spieler) {
+  static spielen(spieler) {
     def karte = spieler.get(random.nextInt(spieler.size()))
     spieler = spieler - karte
     return karte
   }
 
-  def static main(args) {
+  static main(args) {
+
     geben()
     println "Spieler 1: " + spieler1.sort()
     println "Spieler 2: " + spieler2.sort()
