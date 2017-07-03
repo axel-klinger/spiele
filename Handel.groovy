ce0e66875b7426b0534cd5163ce852ceece62f8e
Axel Klinger <axel.klinger@web.de>
2017-07-03 20:01:44 +0200

    Erste Version

---
+++ Handel.groovy
@@ -0,0 +1,14 @@
+class Handel {
+
+  static main(args) {
+
+    println "Handel"
+
+    // 30 tage handeln
+    30.times { number ->
+      println "Tag " + (number + 1)
+    }
+
+    println "Fertig!"
+  }
+}
